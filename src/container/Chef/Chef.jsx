0b006fa863bdc2f2fc825b9f66f56e680cfd5398
unknown <istofar201@gmail.com>
2023-01-10 14:23:51 +0100

    chef

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -8,21 +8,23 @@ const Chef = () => (
   <div className="app__bg app__wrapper section__padding">
     <div className="app__wrapper_img app__wrapper_img-reverse">
       <img src={images.chef} alt="chef" />
-      <div className="app__wrapper_info">
-        <SubHeading title="Chef's Word" />
-        <h1 className="headtext__cormorant">What we believe in</h1>
-        <div className="app__chef-content">
-          <div className="app__chef-content_quote">
-            <img src={images.quote} alt="quote" />
-            <p className="p__opensans">
-              "You must be imaginative, strong hearted. You must try things that
-              may not work, and you must not let anyone define your limits
-              because of where you come from. Your only limit is your soul."
-            </p>
-          </div>
-          <p className="app__chef-sign">Gusteau</p>
-          <p className="p__opensans">Chef & Founder</p>
-          <img src={images.sign} alt="sign" />
+    </div>
+    <div className="app__wrapper_info">
+      <SubHeading title="Chef's Word" />
+      <h1 className="headtext__cormorant">What we believe in</h1>
+      <div className="app__chef-content">
+        <div className="app__chef-content_quote">
+          <img src={images.quote} alt="quote" />
+          <p className="p__opensans">
+            "You must be imaginative, strong hearted. You must try things that
+            may not work, and you must not let anyone define your limits because
+            of where you come from. Your only limit is your soul."
+          </p>
+        </div>
+        <div className="app__chef-sign">
+        <p>Gusteau</p>
+        <p className="p__opensans">Chef & Founder</p>
+        <img src={images.sign} alt="sign" />
         </div>
       </div>
     </div>
